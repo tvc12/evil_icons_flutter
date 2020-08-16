f0db4790a80cd9d858e4bad2afac273c3cde3142
tvc12 - Thien Vi Chi <meomeocf98@gmail.com>
2020-08-16 15:23:07 +0700

    Update: evil icons

--- lib/evil_icons.dart
+++ lib/evil_icons.dart
@@ -1,77 +1,213 @@
 part of evil_icons_flutter;
 
 abstract class EvilIcons {
-  // See https://github.com/tvc12/evil_icons_flutter
-  // See http://evil-icons.io/
+  /// ![archive](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-archive.svg)
+  static const archive = EvilIconData(0xf100);
 
-  static const EvilIconData archive = EvilIconData(0xf100);
-  static const EvilIconData arrow_down = EvilIconData(0xf101);
-  static const EvilIconData arrow_left = EvilIconData(0xf102);
-  static const EvilIconData arrow_right = EvilIconData(0xf103);
-  static const EvilIconData arrow_up = EvilIconData(0xf104);
-  static const EvilIconData bell = EvilIconData(0xf105);
-  static const EvilIconData calendar = EvilIconData(0xf106);
-  static const EvilIconData camera = EvilIconData(0xf107);
-  static const EvilIconData cart = EvilIconData(0xf108);
-  static const EvilIconData chart = EvilIconData(0xf109);
-  static const EvilIconData check = EvilIconData(0xf10a);
-  static const EvilIconData chevron_down = EvilIconData(0xf10b);
-  static const EvilIconData fchevron_left = EvilIconData(0xf10c);
-  static const EvilIconData chevron_right = EvilIconData(0xf10d);
-  static const EvilIconData chevron_up = EvilIconData(0xf10e);
-  static const EvilIconData clock = EvilIconData(0xf10f);
-  static const EvilIconData close = EvilIconData(0xf110);
-  static const EvilIconData close_o = EvilIconData(0xf111);
-  static const EvilIconData comment = EvilIconData(0xf112);
-  static const EvilIconData credit_card = EvilIconData(0xf113);
-  static const EvilIconData envelope = EvilIconData(0xf114);
-  static const EvilIconData exclamation = EvilIconData(0xf115);
-  static const EvilIconData external_link = EvilIconData(0xf116);
-  static const EvilIconData eye = EvilIconData(0xf117);
-  static const EvilIconData gear = EvilIconData(0xf118);
-  static const EvilIconData heart = EvilIconData(0xf119);
-  static const EvilIconData image = EvilIconData(0xf11a);
-  static const EvilIconData like = EvilIconData(0xf11b);
-  static const EvilIconData link = EvilIconData(0xf11c);
-  static const EvilIconData location = EvilIconData(0xf11d);
-  static const EvilIconData lock = EvilIconData(0xf11e);
-  static const EvilIconData minus = EvilIconData(0xf11f);
-  static const EvilIconData navicon = EvilIconData(0xf120);
-  static const EvilIconData paperclip = EvilIconData(0xf121);
-  static const EvilIconData pencil = EvilIconData(0xf122);
-  static const EvilIconData play = EvilIconData(0xf123);
-  static const EvilIconData plus = EvilIconData(0xf124);
-  static const EvilIconData pointer = EvilIconData(0xf125);
-  static const EvilIconData question = EvilIconData(0xf126);
-  static const EvilIconData redo = EvilIconData(0xf127);
-  static const EvilIconData refresh = EvilIconData(0xf128);
-  static const EvilIconData retweet = EvilIconData(0xf129);
-  static const EvilIconData sc_facebook = EvilIconData(0xf12a);
-  static const EvilIconData sc_github = EvilIconData(0xf12b);
-  static const EvilIconData sc_google_plus = EvilIconData(0xf12c);
-  static const EvilIconData sc_instagram = EvilIconData(0xf12d);
-  static const EvilIconData sc_linkedin = EvilIconData(0xf12e);
-  static const EvilIconData sc_odnoklassniki = EvilIconData(0xf12f);
-  static const EvilIconData sc_pinterest = EvilIconData(0xf130);
-  static const EvilIconData sc_skype = EvilIconData(0xf131);
-  static const EvilIconData sc_soundcloud = EvilIconData(0xf132);
-  static const EvilIconData sc_telegram = EvilIconData(0xf133);
-  static const EvilIconData sc_tumblr = EvilIconData(0xf134);
-  static const EvilIconData sc_twitter = EvilIconData(0xf135);
-  static const EvilIconData sc_vimeo = EvilIconData(0xf136);
-  static const EvilIconData sc_vk = EvilIconData(0xf137);
-  static const EvilIconData sc_youtube = EvilIconData(0xf138);
-  static const EvilIconData search = EvilIconData(0xf139);
-  static const EvilIconData share_apple = EvilIconData(0xf13a);
-  static const EvilIconData share_google = EvilIconData(0xf13b);
-  static const EvilIconData spinner = EvilIconData(0xf13c);
-  static const EvilIconData spinner_2 = EvilIconData(0xf13d);
-  static const EvilIconData spinner_3 = EvilIconData(0xf13e);
-  static const EvilIconData star = EvilIconData(0xf13f);
-  static const EvilIconData tag = EvilIconData(0xf140);
-  static const EvilIconData trash = EvilIconData(0xf141);
-  static const EvilIconData trophy = EvilIconData(0xf142);
-  static const EvilIconData undo = EvilIconData(0xf143);
-  static const EvilIconData unlock = EvilIconData(0xf144);
-  static const EvilIconData user = EvilIconData(0xf145);
+  /// ![arrow-down](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-arrow-down.svg)
+  static const arrow_down = EvilIconData(0xf101);
+
+  /// ![arrow-left](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-arrow-left.svg)
+  static const arrow_left = EvilIconData(0xf102);
+
+  /// ![arrow-right](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-arrow-right.svg)
+  static const arrow_right = EvilIconData(0xf103);
+
+  /// ![arrow-up](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-arrow-up.svg)
+  static const arrow_up = EvilIconData(0xf104);
+
+  /// ![bell](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-bell.svg)
+  static const bell = EvilIconData(0xf105);
+
+  /// ![calendar](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-calendar.svg)
+  static const calendar = EvilIconData(0xf106);
+
+  /// ![camera](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-camera.svg)
+  static const camera = EvilIconData(0xf107);
+
+  /// ![cart](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-cart.svg)
+  static const cart = EvilIconData(0xf108);
+
+  /// ![chart](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-chart.svg)
+  static const chart = EvilIconData(0xf109);
+
+  /// ![check](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-check.svg)
+  static const check = EvilIconData(0xf10a);
+
+  /// ![chevron-down](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-chevron-down.svg)
+  static const chevron_down = EvilIconData(0xf10b);
+
+  /// ![fchevron-left](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-fchevron-left.svg)
+  static const fchevron_left = EvilIconData(0xf10c);
+
+  /// ![chevron-right](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-chevron-right.svg)
+  static const chevron_right = EvilIconData(0xf10d);
+
+  /// ![chevron-up](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-chevron-up.svg)
+  static const chevron_up = EvilIconData(0xf10e);
+
+  /// ![clock](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-clock.svg)
+  static const clock = EvilIconData(0xf10f);
+
+  /// ![close](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-close.svg)
+  static const close = EvilIconData(0xf110);
+
+  /// ![close-o](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-close-o.svg)
+  static const close_o = EvilIconData(0xf111);
+
+  /// ![comment](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-comment.svg)
+  static const comment = EvilIconData(0xf112);
+
+  /// ![credit-card](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-credit-card.svg)
+  static const credit_card = EvilIconData(0xf113);
+
+  /// ![envelope](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-envelope.svg)
+  static const envelope = EvilIconData(0xf114);
+
+  /// ![exclamation](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-exclamation.svg)
+  static const exclamation = EvilIconData(0xf115);
+
+  /// ![external-link](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-external-link.svg)
+  static const external_link = EvilIconData(0xf116);
+
+  /// ![eye](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-eye.svg)
+  static const eye = EvilIconData(0xf117);
+
+  /// ![gear](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-gear.svg)
+  static const gear = EvilIconData(0xf118);
+
+  /// ![heart](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-heart.svg)
+  static const heart = EvilIconData(0xf119);
+
+  /// ![image](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-image.svg)
+  static const image = EvilIconData(0xf11a);
+
+  /// ![like](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-like.svg)
+  static const like = EvilIconData(0xf11b);
+
+  /// ![link](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-link.svg)
+  static const link = EvilIconData(0xf11c);
+
+  /// ![location](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-location.svg)
+  static const location = EvilIconData(0xf11d);
+
+  /// ![lock](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-lock.svg)
+  static const lock = EvilIconData(0xf11e);
+
+  /// ![minus](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-minus.svg)
+  static const minus = EvilIconData(0xf11f);
+
+  /// ![navicon](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-navicon.svg)
+  static const navicon = EvilIconData(0xf120);
+
+  /// ![paperclip](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-paperclip.svg)
+  static const paperclip = EvilIconData(0xf121);
+
+  /// ![pencil](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-pencil.svg)
+  static const pencil = EvilIconData(0xf122);
+
+  /// ![play](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-play.svg)
+  static const play = EvilIconData(0xf123);
+
+  /// ![plus](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-plus.svg)
+  static const plus = EvilIconData(0xf124);
+
+  /// ![pointer](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-pointer.svg)
+  static const pointer = EvilIconData(0xf125);
+
+  /// ![question](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-question.svg)
+  static const question = EvilIconData(0xf126);
+
+  /// ![redo](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-redo.svg)
+  static const redo = EvilIconData(0xf127);
+
+  /// ![refresh](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-refresh.svg)
+  static const refresh = EvilIconData(0xf128);
+
+  /// ![retweet](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-retweet.svg)
+  static const retweet = EvilIconData(0xf129);
+
+  /// ![sc-facebook](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-facebook.svg)
+  static const sc_facebook = EvilIconData(0xf12a);
+
+  /// ![sc-github](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-github.svg)
+  static const sc_github = EvilIconData(0xf12b);
+
+  /// ![sc-google-plus](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-google-plus.svg)
+  static const sc_google_plus = EvilIconData(0xf12c);
+
+  /// ![sc-instagram](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-instagram.svg)
+  static const sc_instagram = EvilIconData(0xf12d);
+
+  /// ![sc-linkedin](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-linkedin.svg)
+  static const sc_linkedin = EvilIconData(0xf12e);
+
+  /// ![sc-odnoklassniki](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-odnoklassniki.svg)
+  static const sc_odnoklassniki = EvilIconData(0xf12f);
+
+  /// ![sc-pinterest](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-pinterest.svg)
+  static const sc_pinterest = EvilIconData(0xf130);
+
+  /// ![sc-skype](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-skype.svg)
+  static const sc_skype = EvilIconData(0xf131);
+
+  /// ![sc-soundcloud](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-soundcloud.svg)
+  static const sc_soundcloud = EvilIconData(0xf132);
+
+  /// ![sc-telegram](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-telegram.svg)
+  static const sc_telegram = EvilIconData(0xf133);
+
+  /// ![sc-tumblr](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-tumblr.svg)
+  static const sc_tumblr = EvilIconData(0xf134);
+
+  /// ![sc-twitter](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-twitter.svg)
+  static const sc_twitter = EvilIconData(0xf135);
+
+  /// ![sc-vimeo](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-vimeo.svg)
+  static const sc_vimeo = EvilIconData(0xf136);
+
+  /// ![sc-vk](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-vk.svg)
+  static const sc_vk = EvilIconData(0xf137);
+
+  /// ![sc-youtube](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-sc-youtube.svg)
+  static const sc_youtube = EvilIconData(0xf138);
+
+  /// ![search](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-search.svg)
+  static const search = EvilIconData(0xf139);
+
+  /// ![share-apple](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-share-apple.svg)
+  static const share_apple = EvilIconData(0xf13a);
+
+  /// ![share-google](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-share-google.svg)
+  static const share_google = EvilIconData(0xf13b);
+
+  /// ![spinner](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-spinner.svg)
+  static const spinner = EvilIconData(0xf13c);
+
+  /// ![spinner-2](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-spinner-2.svg)
+  static const spinner_2 = EvilIconData(0xf13d);
+
+  /// ![spinner-3](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-spinner-3.svg)
+  static const spinner_3 = EvilIconData(0xf13e);
+
+  /// ![star](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-star.svg)
+  static const star = EvilIconData(0xf13f);
+
+  /// ![tag](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-tag.svg)
+  static const tag = EvilIconData(0xf140);
+
+  /// ![trash](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-trash.svg)
+  static const trash = EvilIconData(0xf141);
+
+  /// ![trophy](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-trophy.svg)
+  static const trophy = EvilIconData(0xf142);
+
+  /// ![undo](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-undo.svg)
+  static const undo = EvilIconData(0xf143);
+
+  /// ![unlock](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-unlock.svg)
+  static const unlock = EvilIconData(0xf144);
+
+  /// ![user](https://github.com/evil-icons/evil-icons/raw/master/assets/icons/ei-user.svg)
+  static const user = EvilIconData(0xf145);
 }
